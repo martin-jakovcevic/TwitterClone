abbc577bd06455df09142e77ab221ec894401ea7
martin-jakovcevic <martin_jakovcevic@yahoo.com>
2020-07-11 00:35:09 +0200

    Creation of Tweets index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # post 'orders/create'
+  # post '/orders', to: 'orders#create'
+  get '/tweets', to: 'tweets#index'
+
+  root to: 'tweets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
