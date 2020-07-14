b347ef39b5e444df347fc33b1da4e8c431bb0a03
martin-jakovcevic <martin_jakovcevic@yahoo.com>
2020-07-14 05:05:18 +0200

    Added links for back button

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # post 'orders/create'
   # post '/orders', to: 'orders#create'
   get '/tweets', to: 'tweets#index'
-  get '/tweets/:id', to: 'tweets#show'
+  get '/tweets/:id', to: 'tweets#show', as: :tweet
 
   root to: 'tweets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
