16e76c751aab6c80e7dd0d847fb7ffa0532bd017
martin-jakovcevic <martin_jakovcevic@yahoo.com>
2020-07-21 02:17:24 +0200

    added bulma and created nav_bar and footer parthials

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
+  # get '/tweets', to: 'tweets#index'
+  # post '/tweets', to: 'tweets#create'
+  # get '/tweets/new', to: 'tweets#new', as: :new_tweet
+  # get '/tweets/:id', to: 'tweets#show', as: :tweet
+  # get '/tweets/:id/edit', to: 'tweets#edit', as: :edit_tweet
+  # patch '/tweets/:id', to: 'tweets#update'
+  # delete '/tweets/:id', to: 'tweets#destroy'
 
-  get '/tweets', to: 'tweets#index'
-  post '/tweets', to: 'tweets#create'
-
-  get '/tweets/new', to: 'tweets#new', as: :new_tweet
-  get '/tweets/:id', to: 'tweets#show', as: :tweet
-
-  get '/tweets/:id/edit', to: 'tweets#edit', as: :edit_tweet
-  patch '/tweets/:id', to: 'tweets#update'
-
-  delete '/tweets/:id', to: 'tweets#destroy'
-  
   root to: 'tweets#index'
+
+  resources :tweets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
